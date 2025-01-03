bb33d19b7acae2f97bfc304ab31913b6b720a26b
Hugo Farroba <hfarroba@googlemail.com>
2025-01-03 09:46:12 +0100

    Adds profanity-check

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from profanity_check import predict_prob
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Username(BaseModel):
+    text: str
+
+@app.post("/check-username")
+async def check_username(username: Username):
+    score = predict_prob([username.text])[0]
+    
+    return {
+        "text": username.text,
+        "is_inappropriate": score > 0.7,
+        "confidence": float(score)
+    }
